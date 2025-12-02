4126506ff69302839b516b2c0f6cebf52fdded54
Thiago Alves de Moura <122846156+ThAlvesM@users.noreply.github.com>
2025-12-01 21:02:08 -0300

    Add join with dim_classificacao_financeira table

--- transformacao_receitas/analises/origem_recurso_n_vinculado.sql
+++ transformacao_receitas/analises/origem_recurso_n_vinculado.sql
@@ -6,11 +6,13 @@ FROM
 JOIN
     analytics_etl.dim_origem AS dim_origem
     ON fato.id_origem_sk = dim_origem.id_origem_sk 
+JOIN
+    analytics_etl.dim_classificacao_financeira AS dim_classificacao
+    ON fato.id_classificacao_sk = dim_classificacao.id_classificacao_sk
 WHERE 
-    fato.fonte_recurso_nome ILIKE 'RECURSOS NÃO VINCULADOS'
+    dim_classificacao.fonte_recurso_nome ILIKE 'RECURSOS NÃO VINCULADOS'
 GROUP BY
     dim_origem.rubrica_receita_nome
 ORDER BY
     valor_total_arrecadado_livre DESC 
 LIMIT 10;
-
